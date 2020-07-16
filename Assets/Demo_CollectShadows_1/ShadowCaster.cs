9f571254f13e56b56f3de7eda151ce701c6830c3
JerryWen <wen18840839539@gmail.com>
2020-07-16 08:43:24 +0800

    Demo_1_类似Projector

---
+++ Assets/Demo_CollectShadows_1/ShadowCaster.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShadowCaster : MonoBehaviour
+{
+    public int targetSize = 512;
+    public float shadowBias = 0.005f;
+
+    private Camera cam;
+    private RenderTexture depthTarget;
+    
+    private void OnEnable() {
+        UpdateResources();
+    }
+    
+    private void OnValidate() {
+        UpdateResources();
+    }
+
+    private void UpdateResources()
+    {
+        if(cam == null) {
+            cam = GetComponent<Camera>();
+            cam.depth = -1000;
+        }
+
+        if(depthTarget == null || depthTarget.width != targetSize)
+        {
+            int sz = Mathf.Max(targetSize, 16);
+            depthTarget = new RenderTexture(sz, sz, 16, RenderTextureFormat.Depth, RenderTextureReadWrite.Linear);
+            depthTarget.wrapMode = TextureWrapMode.Clamp;
+            depthTarget.filterMode = FilterMode.Bilinear;
+            depthTarget.autoGenerateMips = false;
+            depthTarget.useMipMap = false;
+            cam.targetTexture = depthTarget;
+        }
+    }
+
+    private void OnPostRender()
+    {
+        var bias = new Matrix4x4() {
+            m00 = 0.5f, m01 = 0,    m02 = 0,    m03 = 0.5f,
+            m10 = 0,    m11 = 0.5f, m12 = 0,    m13 = 0.5f,
+            m20 = 0,    m21 = 0,    m22 = 0.5f, m23 = 0.5f,
+            m30 = 0,    m31 = 0,    m32 = 0,    m33 = 1,
+        };
+        
+        Matrix4x4 view = cam.worldToCameraMatrix;
+        Matrix4x4 proj = cam.projectionMatrix;
+        Matrix4x4 mtx = bias * proj * view;
+        
+        Shader.SetGlobalMatrix("_ShadowMatrix", mtx);
+        Shader.SetGlobalTexture("_ShadowTex", depthTarget);
+        Shader.SetGlobalFloat("_ShadowBias", shadowBias);
+    }
+}
---
+++ Assets/Demo_CollectShadows_1/ShadowCollector.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/ShadowCollector" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+        #include "UnityCG.cginc"
+		#pragma surface surf Lambert fullforwardshadows vertex:vert
+		#pragma target 3.0
+        
+        sampler2D _ShadowTex;
+        float4x4 _ShadowMatrix;
+        float _ShadowBias;
+
+        sampler2D _MainTex;
+        
+        struct Input {
+			float2 uv_MainTex;
+            float4 shadowCoords;
+		};
+
+        void vert(inout appdata_full v, out Input o) {
+            UNITY_INITIALIZE_OUTPUT(Input, o);
+            float3 worldPos = mul(unity_ObjectToWorld, float4(v.vertex.xyz, 1.0)).xyz;
+            o.shadowCoords = mul(_ShadowMatrix, float4(worldPos, 1.0));
+        }
+
+		void surf(Input IN, inout SurfaceOutput o)
+        {
+            float lightDepth = 1.0 - tex2Dproj(_ShadowTex, IN.shadowCoords).r;
+            float shadow = (IN.shadowCoords.z - _ShadowBias) < lightDepth ? 1.0 : 0.5;
+
+			float4 c = tex2D(_MainTex, IN.uv_MainTex);
+			o.Albedo = c.rgb * shadow;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
